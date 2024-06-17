4b5f2789e041b510629878fb1769948a28debb3d
Kalu Daniel <krayzhee419@gmail.com>
2024-06-17 23:00:22 +0100

    feat: Added hero section  to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,20 @@ import Navbar from "../components/Navbar";
 export default function Home() {
     return (
         <main>
+            <Navbar />
             <section className="h-screen bg-top bg-cover bg-bg-hero bg-no-repeat w-full">
-                <Navbar />
-                <h2>yo</h2>
+                <div className="flex flex-col gap-y-3.5 w-full top-[50%] items-center gap-3 text-white absolute md:top-[60%] md:left-[4%]  z-20  md:w-[40%]">
+                    <h3 className="font-normal text-3xl md:text-4xl font-Inter">Your Cozy Era</h3>
+                    <p className="font-normal text-lg md:text-2xl text-center">
+                        Get peak comfy-chic <br /> with new winter essentials.
+                    </p>
+                    <button
+                        type="button"
+                        className="uppercase w-[40%] mx-autofont-normal text-sm text-text bg-white py-2 md:py-3"
+                    >
+                        shop now
+                    </button>
+                </div>
             </section>
         </main>
     );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,23 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+
 import flag from "../public/assets/us 1.svg";
 import arrow from "../public/assets/ArrowRight.svg";
+
 import { IoSearchSharp } from "react-icons/io5";
 import { LuUser2 } from "react-icons/lu";
 import { IoCartOutline } from "react-icons/io5";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
+import { useState } from "react";
 
 export const Header = () => {
     return (
-        <div className="flex items-center justify-between bg-black text-white h-auto px-8 py-2 w-full">
+        <div className="flex items-center justify-between flex-col text-center  gap-y-2 md:flex-row bg-black text-white h-auto px-8 py-2 w-full">
             <div />
             <div>
-                <p className="flex items-center text-xs font-semibold gap-x-1.5 tracking-wide ">
+                <p className="flex items-center text-xs font-semibold gap-y-2 md:gap-x-1.5 tracking-wide flex-col md:flex-row ">
                     Get early access on launches and offers.
                     <Link
                         href=""
@@ -34,7 +40,7 @@ export const Header = () => {
 
 export const Submenu = () => {
     return (
-        <div className="flex items-center justify-center gap-4 h-[9vh] border-b border-[#DDDBDC] px-12 bg-white ">
+        <div className="lg:flex items-center justify-center gap-4 h-[9vh] border-b border-[#DDDBDC] px-12 bg-white hidden ">
             <ul className="flex items-center gap-x-4 h-[9vh]">
                 <li className="font-normal text-xs flex items-center  border-b-2 border-transparent hover:border-b-2 hover:border-black h-full transition ease-in text-text  tracking-wide">
                     <Link href={""}>Holiday Gifting</Link>
@@ -69,11 +75,67 @@ export const Submenu = () => {
 };
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    function ShowNav() {
+        setNav(true);
+    }
+
+    function CloseNav() {
+        setNav(false);
+    }
     return (
         <section className="w-screen font-inter">
             <Header />
 
-            <div className="flex items-center justify-between gap-4 h-[9vh] border-b border-[#DDDBDC] px-12 bg-white">
+            <div className="flex items-center justify-between gap-4 h-[9vh] border-b border-[#DDDBDC] px-3 bg-white lg:hidden">
+                <button type="button" onClick={ShowNav}>
+                    <CiMenuBurger className="text-text text-xl" />
+                </button>
+
+                <div className="ml-[9vw]">
+                    <p className=" font-bold font-inter text-sm -tracking-widest text-text ">
+                        K H A Y - C A R T
+                    </p>
+                </div>
+
+                <div className="flex items-center gap-x-2">
+                    <button type="button">
+                        <IoSearchSharp className="text-text text-xl" />
+                    </button>
+                    <button type="button">
+                        <LuUser2 className="text-text text-xl" />
+                    </button>
+                    <button type="button">
+                        <IoCartOutline className="text-text text-xl" />
+                    </button>
+                </div>
+            </div>
+            <div />
+            {nav && (
+                <div className="absolute top-[10vh] bg-white w-full p-3">
+                    <div className="flex flex-col gap-y-5">
+                        <button type="button" onClick={CloseNav} className="pt-5">
+                            <IoIosClose className="text-text text-3xl" />
+                        </button>
+                        <ul className="flex flex-col gap-y-5">
+                            <li className="hover:underline text-text tracking-wide h-full font-medium text-sm flex items-center transition ease-in">
+                                <Link href={""}>Women</Link>
+                            </li>
+                            <li className="hover:underline text-text tracking-wide h-full font-medium text-sm flex items-center transition ease-in">
+                                <Link href={""}>Men</Link>
+                            </li>
+                            <li className="hover:underline text-text tracking-wide h-full font-medium text-sm flex items-center transition ease-in">
+                                <Link href={""}>About</Link>
+                            </li>
+                            <li className="hover:underline text-text tracking-wide h-full font-medium text-sm flex items-center transition ease-in">
+                                <Link href={""}>Everworld Stories</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            )}
+            
+            <div className="items-center justify-between gap-4 h-[9vh] border-b border-[#DDDBDC] px-12 bg-white hidden lg:flex">
                 <ul className="flex items-center gap-x-4 h-[9vh]">
                     <li className="font-normal text-xs flex items-center  border-b-2 border-transparent hover:border-b-2 hover:border-black h-full transition ease-in text-text tracking-wide">
                         <Link href={""}>Women</Link>
