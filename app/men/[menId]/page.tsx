bde9d473d6f336f23de6fd8b7c6217222c4927b9
Kalu Daniel <krayzhee419@gmail.com>
2024-06-21 22:52:29 +0100

    Refactor men page to use menId parameter and update styling

--- app/men/[menId]/page.tsx
+++ app/men/[menId]/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "../../../components/ui/Footer";
 import Navbar from "../../../components/ui/Navbar";
-
+import React from "react";
 const page = ({ params }: any) => {
     console.log(params.menId);
 
@@ -8,7 +8,7 @@ const page = ({ params }: any) => {
         <div>
             <Navbar />
             <div className="flex flex-col justify-between gap-y-10 lg:flex-row w-11/12 mx-auto my-20">
-                <h1>This is me {params.menId}</h1>
+                <h1>This is the {params.menId}</h1>
             </div>
             <Footer />
         </div>
--- app/men/page.tsx
+++ app/men/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "../../components/ui/Navbar";
 import Footer from "../../components/ui/Footer";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
